4a1f3eb7148c0943d78ad981c9010ba3b3fefa4c
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2023-11-28 07:07:18 +0530

    script.js

--- 54 INTRODUCTION/script.js
+++ 54 INTRODUCTION/script.js
@@ -1,1 +1,18 @@
 alert("Hello World this is my first Java Script program.")
+
+
+console.log("Code is running.")
+// Here this runs when you inspect a page and check the console menu in web browser.
+
+var firstvariable=prompt("Enter your first variable:")
+console.log("Your number in first variable:"+ firstvariable )
+
+// Here confirm is used which gets values either TRUE or FALSE.
+var isTrue=confirm("You want to blast your computer?")
+if (isTrue)
+{
+    console.log("It is blasting.")
+}
+else{
+    console.log("It is not blasting.")
+}
